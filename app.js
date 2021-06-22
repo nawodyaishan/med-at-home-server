8a09b3da131243fcc7c8425c8baa02758e8c990f
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2021-06-23 01:05:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,10 @@ mongoose
 
 
 //Production
+/*
 var server = app.listen(process.env.PORT || 3000, function () {
   var port = server.address().port;
   console.log("Express is working on port " + port)
 })
+*/
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
